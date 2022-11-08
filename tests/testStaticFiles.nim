f7102b345521a78bde2c0527e157005d41064f8c
Jake Leahy <jake@leahy.dev>
2022-11-08 11:11:18 +1100

    Fix line endings

--- tests/testStaticFiles.nim
+++ tests/testStaticFiles.nim
@@ -1,1 +1,27 @@
-import mikeimport unittest, osimport utilsservePublic("tests/public", "static")runServerInBackground()suite "Static files":  let    testFilePath = "tests/public/test.html"    testHtml = testFilePath.readFile()  # Remove file to double check that files  # are getting read from memory  removeFile testFilePath  test "Can get file":    let resp = get("/static/test.html")    check resp.code == Http200    check resp.body == testHtml  test "404 when file doesn't exist":    check get("/static/doesntExist").code == Http404  # TODO: Add tests for compression  # Write back to future tests dont fail  testFilePath.writeFile(testHtml)
+import mike
+import unittest, os
+import utils
+
+
+servePublic("tests/public", "static")
+runServerInBackground()
+
+
+suite "Static files":
+  let
+    testFilePath = "tests/public/test.html"
+    testHtml = testFilePath.readFile()
+  # Remove file to double check that files
+  # are getting read from memory
+  removeFile testFilePath
+  test "Can get file":
+    let resp = get("/static/test.html")
+    check resp.code == Http200
+    check resp.body == testHtml
+
+  test "404 when file doesn't exist":
+    check get("/static/doesntExist").code == Http404
+    # TODO: Add tests for compression
+
+  # Write back to future tests dont fail
+  testFilePath.writeFile(testHtml)
