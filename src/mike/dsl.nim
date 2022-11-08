3595abbc7116312c60869a1d626fb5821f672c5e
Jake Leahy <jake@leahy.dev>
2022-11-08 14:55:24 +1100

    Return instead of break

--- src/mike/dsl.nim
+++ src/mike/dsl.nim
@@ -188,7 +188,7 @@ proc onRequest(req: Request): Future[void] {.async.} =
               status: code
             ))
             # We shouldn't continue after errors so stop processing
-            break
+            return
         else:
           # TODO: Remove the ability to return string. Benchmarker still uses return statement
           # style so guess I'll keep it for some time
