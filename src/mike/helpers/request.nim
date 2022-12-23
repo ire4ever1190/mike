46423d087f2346dd413e99e055c6746671fbb854
Jake Leahy <jake@leahy.dev>
2022-12-23 19:05:57 +1100

    Add authentication related procs (#11)
    Add support for working with
    - HTTP basic auth
    - HTTP Bearer auth
    Can build on top of these later

--- src/mike/helpers/request.nim
+++ src/mike/helpers/request.nim
@@ -3,20 +3,22 @@ import httpx
 import std/json
 import std/jsonutils
 import std/options
+import std/selectors
+
 
 {.used.}
 
-proc body*(ctx: Context): string =
+
+proc optBody*(ctx: Context): Option[string] {.inline, raises: [].} =
+    ## Returns the request body from the request
+    try: ctx.request.body except IOSelectorsException, ValueError: none(string)
+
+proc body*(ctx: Context): string {.inline, raises: [].}=
     ## Gets the request body from the request
-    ## Returns an empty string if the user sent no body
-    ctx.request.body.get("")
+    ## Returns an empty string if the user sent no bodyt
+    ctx.optBody.get("")
 
-proc optBody*(ctx: Context): Option[string] =
-    ## Returns the request body from the request.
-    ## Not really that useful but exists if you want to use it
-    ctx.request.body
-
-proc hasBody*(ctx: Context): bool =
+proc hasBody*(ctx: Context): bool {.raises: [].} =
   ## Returns `true` if the request has a body
   result = ctx.body != ""
 
@@ -32,28 +34,30 @@ proc json*[T](ctx: Context, to: typedesc[T]): T =
         allowMissingKeys: false
     ))
 
+proc headers*(ctx: Context): HttpHeaders {.raises: [].} =
+  ## Returns Headers for a request
+  try:
+    ctx.request.headers.get(newHttpHeaders())
+  except KeyError, IOSelectorsException:
+    newHttpHeaders()
+
 proc getHeader*(ctx: Context, key: string): string =
     ## Gets a header from the request with `key`
     ctx.request.headers.get()[key]
 
 proc getHeaders*(ctx: Context, key: string): seq[string] =
   ## Returns all values for a header. Use this if the request contains multiple
-  ## headers with the same key
-  (seq[string])(ctx.request.headers.get()[key])
+  ## headers with the same key. Returns empty if header doesn't exist
+  result = (seq[string])(ctx.headers.getOrDefault(key))
 
 proc getHeader*(ctx: Context, key, default: string): string =
     ## Gets a header from the request with `key` and returns `default`
     ## if it cannot be found
-    let headers = ctx.request.headers
-    if headers.isSome:
-        result = $headers.get().getOrDefault(key, @[default].HttpHeaderValues):
-    else:
-        result = default
+    result = $ctx.headers.getOrDefault(key, @[default].HttpHeaderValues)
 
-proc hasHeader*(ctx: Context, key: string): bool =
+proc hasHeader*(ctx: Context, key: string): bool {.raises: [].} =
     ## Returns true if the request has header with `key`
-    if ctx.request.headers.isSome:
-        result = ctx.request.headers.get().hasKey(key)
+    result = ctx.headers.hasKey(key)
 
 proc httpMethod*(ctx: Context): HttpMethod =
   ## Returns the HTTP method of a request
---
+++ tests/testAuth.nim
@@ -0,0 +1,81 @@
+import mike
+import utils
+
+import std/[
+  unittest,
+  json,
+  httpclient,
+  strformat,
+  strutils,
+  base64,
+  httpcore
+]
+
+"/basic" -> get():
+  discard ctx.basicAuth("foo", "bar")
+  ctx.send "Hello authenticated user"
+
+"/scheme" -> get(scheme: AuthScheme):
+  ctx.send $scheme
+
+"/bearer" -> get():
+  ctx.send ctx.bearerToken
+
+runServerInBackground()
+
+suite "Basic":
+  proc authHeader(username, password: string): seq[(string, string)] =
+    result = @{"Authorization": "Basic " & encode(username & ":" & password)}
+  test "No header 401":
+    let resp = get("/basic")
+    check resp.code == Http401
+    check resp.headers["WWW-Authenticate"] == "Basic realm=Enter details"
+
+  test "Bad header 400":
+    check get("/basic", {"Authorization": "B"}).code == Http400
+
+  test "Malformed header 400":
+    check get("/basic", {"Authorization": "Basic " & encode("userpass")}).code == Http400
+
+  test "Invalid username 401":
+    let resp = get("/basic", authHeader("f", "bar"))
+    check resp.code == Http401
+    check resp.headers["WWW-Authenticate"] == "Basic realm=Enter details"
+
+  test "Invalid password 401":
+    let resp = get("/basic", authHeader("foo", "b"))
+    check resp.code == Http401
+    check resp.headers["WWW-Authenticate"] == "Basic realm=Enter details"
+
+  test "Valid credentials":
+    let resp = get("/basic", authHeader("foo", "bar"))
+    check resp.code == Http200
+    check not resp.headers.hasKey("WWW-Authenticate")
+    check resp.body == "Hello authenticated user"
+
+suite "Bearer":
+  test "Can get token":
+    let resp = get("/bearer", {"Authorization": "Bearer 123456789"})
+    check:
+      resp.code == Http200
+      resp.body == "123456789"
+
+  test "Error when not bearer":
+    let resp = get("/bearer", {"Authorization": "123456789"})
+    check resp.code == Http400
+
+  test "Error when no auth header":
+    let resp = get("/bearer")
+    check resp.code == Http401
+
+suite "Utils":
+  test "Can get scheme":
+    let resp = get("/scheme", {"Authorization": "Bearer sgnsodnsdiovnsv"})
+    check:
+      resp.code == Http200
+      resp.body == "Bearer"
+
+  test "Error if no Auth header":
+    let resp = get("/scheme")
+    check resp.code == Http400
+
