84401aa62e4fbd99583d7c70d7487a582789dcfe
Jake Leahy <jake@leahy.dev>
2024-08-10 19:27:02 +1000

    Default path to site wide since thats most expected
    Also is how flask does it

--- src/mike/cookies.nim
+++ src/mike/cookies.nim
@@ -68,14 +68,14 @@ proc `$`*(c: SetCookie): string {.raises: [].} =
   if c.httpOnly: result &= "; HttpOnly"
   result &= "; SameSite=" & $c.sameSite
 
-func initCookie*(name, value: string, domain = "", path = "", secure = false,
+func initCookie*(name, value: string, domain = "", path = "/", secure = false,
                 httpOnly = false, sameSite = Lax): SetCookie  {.inline, raises: [].}=
   ## Creates a new session cookie, see [MDN Set-Cookie](https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers/Set-Cookie) docs
   ## for explanation about the values
   result = SetCookie(name: name, value: value, domain: domain, path: path,
                      secure: secure, httpOnly: httpOnly, sameSite: sameSite)
 
-func initCookie*(name, value: string, maxAge: TimeInterval, domain = "", path = "", secure = false,
+func initCookie*(name, value: string, maxAge: TimeInterval, domain = "", path = "/", secure = false,
                 httpOnly = false, sameSite = Lax): SetCookie {.inline, raises: [].} =
   ## Creates a new cookie that only lasts for a set amount of time
   ##
@@ -83,7 +83,7 @@ func initCookie*(name, value: string, maxAge: TimeInterval, domain = "", path = 
   result = initCookie(name, value, domain, path, secure, httpOnly, sameSite)
   result.maxAge = some maxAge
 
-func initCookie*(name, value: string, expires: DateTime, domain = "", path = "", secure = false,
+func initCookie*(name, value: string, expires: DateTime, domain = "", path = "/", secure = false,
                 httpOnly = false, sameSite = Lax): SetCookie {.inline, raises: [].} =
   ## Creates a new cookie that only lasts until **expires**
   ##
