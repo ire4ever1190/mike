9d51db65146486ae53251ddc6f78a8931ad2c472
Jake Leahy <jake@leahy.dev>
2024-10-29 18:57:01 +1100

    Use two space indentation

--- example.nim
+++ example.nim
@@ -7,35 +7,35 @@ import strformat
 #
 
 "/" -> get:
-    ctx.send "Mike is running!"
+  ctx.send "Mike is running!"
 
 "/hello" -> post:
-    let names = ctx.json(seq[string])
-    for name in names:
-        echo name
-    ctx.send "OK"
+  let names = ctx.json(seq[string])
+  for name in names:
+    echo name
+  ctx.send "OK"
 
 "/shutdown" -> get:
-    quit 0
+  quit 0
 
 #
 # Custom data
 #
 
 type
-    Person = ref object of RootObj
-        name: string
-        age: int
+  Person = ref object of RootObj
+    name: string
+    age: int
 
 "/person/:name/:age" -> beforeGet:
-    ctx &= Person(
-        name: ctx.pathParams["name"],
-        age: ctx.pathParams["age"].parseInt()
-    )
+  ctx &= Person(
+    name: ctx.pathParams["name"],
+    age: ctx.pathParams["age"].parseInt()
+  )
 
 "/person/:name/:age" -> get:
-    let person = ctx[Person]
-    ctx.send fmt"Hello {person.name} aged {person.age}"
+  let person = ctx[Person]
+  ctx.send fmt"Hello {person.name} aged {person.age}"
 
 
 run()
