4d315af393a843e63782d48f060cb5f6e9d03ea6
Jake Leahy <jake@leahy.dev>
2025-03-15 11:41:28 +1100

    Can now compile a test with a single route

--- src/mike/app.nim
+++ src/mike/app.nim
@@ -1,5 +1,9 @@
-import ./[router, context, common]
-import std/[httpcore, macros]
+import router, context, common, errors
+import helpers/[response, context]
+
+import std/[httpcore, macros, options, asyncdispatch, parseutils, strtabs, terminal, cpuinfo]
+
+import httpx
 
 ## This is a fully typesafe version of the API. All responses should be
 ## done by returning like a normal function. For getting data, [Context]
@@ -18,9 +22,64 @@ type
 
 using mapp: var App
 
-proc initApp(): App =
+proc initApp*(): App =
   ## Creates a new Mike app.
   return App()
+
+func getPathAndQuery(url: sink string): tuple[path, query: string] {.inline.} =
+  ## Returns the path and query string from a url
+  let pathLength = url.parseUntil(result.path, '?')
+  # Add query string that comes after
+  if pathLength != url.len():
+      result.query = url[pathLength + 1 .. ^1]
+
+proc extractEncodedParams(input: sink string, table: var StringTableRef) {.inline.} =
+  ## Extracts the parameters into a table
+  for (key, value) in common.decodeQuery(input):
+    table[key] = value
+
+proc makeOnRequest(app: App): OnRequest {.inline.} =
+  proc onRequest(req: Request): Future[void] {.async.} =
+    ## Handles running the requests
+    {.gcsafe.}:
+      if req.path.isSome() and req.httpMethod.isSome():
+        var foundMain = false
+        let
+          ctx = req.newContext()
+          (path, query) = req.path.unsafeGet().getPathAndQuery()
+        extractEncodedParams(query, ctx.queryParams)
+        for routeResult in app.router.route(req.httpMethod.unsafeGet(), path, foundMain):
+          ctx.pathParams = routeResult.pathParams
+          # Run the future then manually handle any error
+          var fut = routeResult.handler(ctx)
+          yield fut
+          if fut.failed:
+              when not defined(release):
+                stderr.styledWriteLine(
+                    fgRed, "Error while handling: ", $req.httpMethod.get(), " ", req.path.get(),
+                    "\n" ,fut.error[].msg, "\n", fut.error.getStackTrace(),
+                    resetStyle
+                )
+              ctx.handled = false
+              # await handleRequestError(fut.error, ctx)
+              # We shouldn't continue after errors so stop processing
+              return
+
+        if not foundMain and not ctx.handled:
+          ctx.handled = false
+          ctx.send(ProblemResponse(
+            kind: "NotFoundError",
+            detail: path & " could not be found",
+            status: Http404
+          ))
+
+        elif not ctx.handled:
+          # Send response if user set response properties but didn't send
+          ctx.send(ctx.response.body, ctx.response.code)
+
+      else:
+        req.send("This request is malformed", Http400)
+  return onRequest
 
 proc internalMap(mapp; verbs: set[HttpMethod], path: string, position: HandlerPos, handler: Handler) =
   ## Internal function for mapping a handler into the [App]. This is called after a handler
@@ -43,34 +102,30 @@ macro wrapProc(x: proc): AsyncHandler =
 
   # Build a proc that just calls all the hooks and then calls the original proc
   result = newProc(
-    params=[parseExpr"Future[string]", newIdentDefs(ctxIdent, ident"Context")],
+    params=[parseExpr"Future[string]", newIdentDefs(ctxIdent, bindSym"Context")],
     pragmas = nnkPragma.newTree(ident"async"),
     body = newStmtList(innerCall)
   )
   echo result.toStrLit
 
-proc map[P: proc](mapp; verbs: set[HttpMethod], path: string, position: HandlerPos, handler: P) =
+proc map*[P: proc](mapp; verbs: set[HttpMethod], path: string, position: HandlerPos, handler: P) =
   ## Low level function for adding a handler into the router. Handler gets transformed
   ## According to parameters/return
   mapp.internalMap(verbs, path, position, wrapProc(handler))
 
-# proc map(mapp; verbs: set[HttpMethod], path: string, handler: proc) =
-  # Maps a function to a set of verbs.
-  # createHandler(handler)
 
-var app = App()
+proc run*(app: var App, port: int = 8080, threads: Natural = 0, bindAddr: string = "0.0.0.0") {.gcsafe.} =
+  ## Starts the server, should be called after you have added all your routes
+  app.router.rearrange()
+  when compileOption("threads"):
+    # Use all processors if the user has not specified a number
+    let threads = if threads > 0: threads else: countProcessors()
 
-import ./[ctxhooks, helpers]
-import asyncdispatch
-
-
-app.map({HttpGet}, "/test", Middle) do (x: Cookie[int]) -> string:
-  echo x
-
-
-when false:
-  app.map({HttpGet}, "/") do (y: Header[string]) -> string:
-    return y
-
-
+  echo "Started server \\o/ on " & bindAddr & ":" & $port
+  let settings = initSettings(
+      Port(port),
+      bindAddr = bindAddr,
+      numThreads = threads
+  )
+  run(makeOnRequest(app), settings)
 
--- src/mike/dsl.nim
+++ src/mike/dsl.nim
@@ -7,6 +7,7 @@ import common
 import helpers/context as contextHelpers
 import helpers/response as responseHelpers
 import errors
+import app
 
 when not defined(release):
   import std/terminal
@@ -26,7 +27,7 @@ import std/[
 ]
 
 ##[
-  Mike's DSL for routing is pretty simple.
+  This contains the simple old style DSL for writing handlers.
 ]##
 
 runnableExamples:
@@ -43,14 +44,8 @@ runnableExamples:
     end of a path
 ]##
 
-type Route = AsyncHandler
-
-var mikeRouter = Router[Route]()
-
-proc addHandler(path: string, verbs: set[HttpMethod], pos: HandlerPos, handler: AsyncHandler) =
-    ## Adds a handler to the routing table
-    mikeRouter.map(verbs, path, handler, pos)
-
+var http = initApp()
+  ## Global app instance that we map everything to
 
 macro addMiddleware*(path: static[string], verb: static[HttpMethod], pos: static[HandlerPos], handler: AsyncHandler) =
     ## Adds a middleware to a path
@@ -62,7 +57,7 @@ macro addMiddleware*(path: static[string], verb: static[HttpMethod], pos: static
     # Get the type of the context parameter
 
     result = quote do:
-        addHandler(`path`, HttpMethod(`verb`), HandlerType(`pos`), `handler`)
+        http.addHandler(`path`, HttpMethod(`verb`), HandlerType(`pos`), `handler`)
 
 macro `->`*(path: static[string], info: untyped, body: untyped): untyped =
     ## Defines the operator used to create a handler
@@ -74,10 +69,21 @@ macro `->`*(path: static[string], info: untyped, body: untyped): untyped =
         echo "hello"
     #==#
     let info = getHandlerInfo(path, info, body)
-    let handlerProc = createAsyncHandler(body, info.path, info.params)
 
-    result = genAst(path = info.path, verbs = info.verbs, pos = info.pos, handlerProc):
-        addHandler(path, verbs, pos, handlerProc)
+    # Build list of parameters from the info
+    var params = @[
+      parseExpr"string | void" # Small optimisation to remove string, might need to remove
+    ]
+    for param in info.params:
+      params &= newIdentDefs(ident param.name, param.kind)
+
+    let prc = newProc(
+      params=params,
+      body = body
+    )
+
+    result = genAst(path = info.path, verbs = info.verbs, pos = info.pos, prc):
+        addHandler(path, verbs, pos, prc)
 
 func noAsyncMsg(input: sink string): string {.inline.} =
   ## Removes the async traceback from a message
@@ -115,93 +121,6 @@ macro `->`*(error: typedesc[CatchableError], info, body: untyped) =
   #==#
   if info.kind != nnkIdent and not info.eqIdent("thrown"):
     "Verb must be `thrown`".error(info)
-  let
-    name = $error
-    handlerProc = body.createAsyncHandler("/", @[])
-
-  result = nnkMethodDef.newTree(
-    ident"handleRequestError",
-    newEmptyNode(),
-    newEmptyNode(),
-    nnkFormalParams.newTree(
-      newEmptyNode(),
-      # Pass the error
-      nnkIdentDefs.newTree(
-        ident"error",
-        nnkRefTy.newTree(error),
-        newEmptyNode()
-      ),
-      # And also pass the context
-      newIdentDefs(ident"ctx", ident"Context")
-    ),
-    nnkPragma.newTree(
-      ident"async"
-    ),
-    newEmptyNode(),
-    body
-  )
-
-func getPathAndQuery(url: sink string): tuple[path, query: string] {.inline.} =
-    ## Returns the path and query string from a url
-    let pathLength = url.parseUntil(result.path, '?')
-    # Add query string that comes after
-    if pathLength != url.len():
-        result.query = url[pathLength + 1 .. ^1]
-
-proc extractEncodedParams(input: sink string, table: var StringTableRef) {.inline.} =
-  ## Extracts the parameters into a table
-  for (key, value) in common.decodeQuery(input):
-    table[key] = value
-
-proc onRequest(req: Request): Future[void] {.async.} =
-  {.gcsafe.}:
-    if req.path.isSome() and req.httpMethod.isSome():
-      var
-        foundMain = false
-      let
-        ctx = req.newContext()
-        (path, query) = req.path.unsafeGet().getPathAndQuery()
-      extractEncodedParams(query, ctx.queryParams)
-      for routeResult in mikeRouter.route(req.httpMethod.unsafeGet(), path, foundMain):
-        ctx.pathParams = routeResult.pathParams
-        # Run the future then manually handle any error
-        var fut = routeResult.handler(ctx)
-        yield fut
-        if fut.failed:
-            when not defined(release):
-              stderr.styledWriteLine(
-                  fgRed, "Error while handling: ", $req.httpMethod.get(), " ", req.path.get(),
-                  "\n" ,fut.error[].msg, "\n", fut.error.getStackTrace(),
-                  resetStyle
-              )
-            ctx.handled = false
-            await handleRequestError(fut.error, ctx)
-            # We shouldn't continue after errors so stop processing
-            return
-        else:
-          # TODO: Remove the ability to return string. Benchmarker still uses return statement
-          # style so guess I'll keep it for some time
-          let body = fut.read
-          if body != "":
-            ctx.response.body = body
-
-      if not foundMain and not ctx.handled:
-        ctx.handled = false
-        ctx.send(ProblemResponse(
-          kind: "NotFoundError",
-          detail: path & " could not be found",
-          status: Http404
-        ))
-
-      elif not ctx.handled:
-        # Send response if user set response properties but didn't send
-        ctx.send(ctx.response.body, ctx.response.code)
-
-    else:
-      req.send("This request is malformed", Http400)
-
-
-
 
 proc run*(port: int = 8080, threads: Natural = 0, bindAddr: string = "0.0.0.0") {.gcsafe.} =
     ## Starts the server, should be called after you have added all your routes
