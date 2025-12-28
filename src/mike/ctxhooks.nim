11f9d6537d1d5e00456617a1e1de13ace4c5848e
Jake Leahy <jake@leahy.dev>
2025-12-28 18:51:15 +1100

    Add a test, this doesn't get the codegen error we got in other project but gets the error we got after the fix

--- src/mike/ctxhooks.nim
+++ src/mike/ctxhooks.nim
@@ -411,7 +411,7 @@ proc sendResponse*[T: not void](ctx: Context, resp: T) =
   ## Generic send hook that delegates to `ctx.send`
   ctx.send(resp)
 
-proc sendResponse*(ctx: Context, stmt: void) =
+proc sendResponse*[T: void](ctx: Context, stmt: T) =
   ## Support for routes that return nothing. Just
   ## sends a 200 response
   discard
